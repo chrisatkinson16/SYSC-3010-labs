33dda79c22c21a555023d14ebbc2ebadac274c51
Chris Atkinson <56167833+chrisatkinson16@users.noreply.github.com>
2020-09-25 18:48:36 -0400

    Update lab2-thingspeek-step1.py

--- lab2-thingspeek-step1.py
+++ lab2-thingspeek-step1.py
@@ -12,12 +12,12 @@ def thermometer():
         try:
             conn.request("POST", "/update", params, headers)
             response = conn.getresponse()
-            print temp
-            print response.status, response.reason
+            print (temp)
+            print (response.status, response.reason)
             data = response.read()
             conn.close()
         except:
-            print "connection failed"
+            print ("connection failed")
         break
 if __name__ == "__main__":
         while True:
